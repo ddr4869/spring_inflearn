f853dbb645d06fb957a56ea994a94c58dcc71170
ddr4869 <bikdu@naver.com>
2023-12-07 15:07:16 +0900

    ~section3: 객체지향 원리 적용

--- core/src/main/java/hello/core/member/Member.java
+++ core/src/main/java/hello/core/member/Member.java
@@ -4,23 +4,18 @@ public class Member {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public Grade getGrade() {
         return grade;
     }
-
     public void setGrade(Grade grade) {
         this.grade = grade;
     }
